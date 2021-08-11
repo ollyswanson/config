bd6cd8e1ec71ea91c6fdcd4983c352c962dd8423
Olly Swanson <olly.swanson95@gmail.com>
2021-08-11 20:26:36 +0100

    work refactor

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -20,3 +20,11 @@ setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
 setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
 setenv LESS_TERMCAP_ue \e'[0m'           # end underline
 setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+# nvm / node stuff
+function nvm
+   bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+end
+
+set -x NVM_DIR ~/.nvm
+nvm use default --silent
